0d0692b0dfd811cf8ff1f4d431cd65c985eafa62
amin266 <kyubivirus266@gmail.com>
2022-02-09 03:34:10 +0330

    git track 2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,8 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
+from . import views
 
-
+app_name='catalog'
 urlpatterns = [
-    
+    path(r'', views.index, name='index'),
+    re_path(r'^$', views.index, name='index')
 ]
